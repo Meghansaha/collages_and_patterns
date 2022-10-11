ef398e96f5b490e92a2350d41b32c30d109cd4a2
harrism1 <harrism1@mskcc.org>
2022-10-10 20:22:38 -0400

    updating

---
+++ example_scripts/05_live_code.R
@@ -0,0 +1,9 @@
+#====Saving for the live code===#
+library(tidyverse)
+
+#Some colors I want to use#
+retro_blues <- c("#12012E", "#144267", "#15698C", "#0695AA", "#00F3FF")
+
+retro_brights <- c("#F6BF07", "#F67C21", "#ED155A", "#F61867")
+
+retro_purples <- c("#0F061B", "#1D0B35", "#350A3C", "#471049", "#881F74")
